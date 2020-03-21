dfc92a98c4e482f45e86920ed0879918ec33e18a
Henrique <jose.henrique@vsoft.com.br>
2020-03-21 12:55:03 -0300

    validações com yup

--- src/app/controllers/AuthController.js
+++ src/app/controllers/AuthController.js
@@ -2,8 +2,17 @@ import jwt from 'jsonwebtoken';
 import authConfig from '../../config/auth';
 import Pet from '../models/Pet';
 
+import * as Yup from 'yup';
+
 class AuthController {
     async store(req, res){
+        const schema = Yup.object().shape({
+            email: Yup.string().required(),
+            password: Yup.string().required()
+        });
+        if(!(await schema.isValid(req.body))){
+            return res.status(400).json({error: 'Alguns campos são obrigatórios'});
+        }
         const { email, password } = req.body;
 
         const pet = await Pet.findOne({
--- src/app/controllers/PetController.js
+++ src/app/controllers/PetController.js
@@ -1,7 +1,17 @@
 import Pet from '../models/Pet';
+import * as Yup from 'yup';
 
 class PetController {
     async store(req, res){
+        const schema = Yup.object().shape({
+            firstName: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password: Yup.string().required().min(6)
+        });
+
+        if(!(await schema.isValid(req.body))){
+            return res.status(400).json({error: 'Alguns campos são obrigatórios'});
+        }
         const petExists = await Pet.findOne({
             where: {
                 email: req.body.email
@@ -21,6 +31,21 @@ class PetController {
     }
 
     async update(req, res){
+        const schema = Yup.object().shape({
+            firstName: Yup.string(),
+            email:  Yup.string().email(),
+            oldPassword: Yup.string().min(6),
+            password: Yup.string().min(6)
+            .when('oldPassword', (oldPassword, field) =>
+            oldPassword ? field.required() : field),
+            confirmPassword: Yup.string().when('password', (password, field) => {
+                password ? field.required().oneOf([Yup.ref('password')]) : field
+            })
+        });
+        if(!(await schema.isValid(req.body))){
+            return res.status(400).json({error: 'Alguns campos são obrigatórios'});
+        }
+
         const { email, oldPassword } = req.body;
 
         const pet = await Pet.findByPk(req.userId);
