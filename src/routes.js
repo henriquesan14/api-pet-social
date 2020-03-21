62cbea629fc673dfd41b123e6028c09974f05ca4
Henrique <jose.henrique@vsoft.com.br>
2020-03-21 11:53:26 -0300

    middleware de autenticação

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,11 @@ import { Router } from 'express';
 import PetController from './app/controllers/PetController';
 import AuthController from './app/controllers/AuthController';
 
+import authMiddleware from './app/middlewares/auth'
+
 const routes = new Router();
 
-routes.post('/pets', PetController.store);
+routes.post('/pets', authMiddleware, PetController.store);
 routes.post('/login', AuthController.store)
 
 export default routes;
